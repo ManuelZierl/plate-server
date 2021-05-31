dc3c23850dd3b0e1a92a4ed62aa2b076d67caed6
ManuelZierl <manuel.zierl@web.de>
2021-05-31 16:26:45 +0200

    added tests

---
+++ test_app.py
@@ -0,0 +1,69 @@
+import unittest
+from app import app, db
+from utils import is_german_plate, levenshtein_distance
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        app.config["SQL_ALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+        self.client = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_plate_post_and_get(self):
+        cases = [
+            ["M-ZG134", True],
+            ["MHL-H8866", True],
+            ["AÖ-Ih4", True],
+            ["QLB-kL77", True],
+            ["M-Ä123", False],  # umlaut after the hyphen
+            ["1-ZG123", False],  # numbers at start
+            ["M-ZG023", False],  # numbers start with 0
+            ["MZG123", False],  # hyphen missing
+        ]
+
+        for plate_str, valid in cases:
+            response = self.client.post("/plate", data={"plate": plate_str})
+            self.assertEqual(response.status_code, 200 if valid else 422)
+            json_response = response.get_json()
+            if valid:
+                self.assertEqual(json_response.get("plate"), plate_str)
+            self.assertEqual(json_response.get("success"), valid)
+
+        response = self.client.get("/plate")
+        json_response = response.get_json()
+        self.assertEqual(len(json_response), 4)
+        self.assertEqual(set(x["plate"] for x in json_response), {
+            "M-ZG134",
+            "MHL-H8866",
+            "AÖ-Ih4",
+            "QLB-kL77"
+        })
+
+
+class TestUtils(unittest.TestCase):
+    def test_is_german_plate(self):
+        self.assertTrue(is_german_plate("MÜR-AB1234"))
+        self.assertTrue(is_german_plate("N-C33"))
+        self.assertTrue(is_german_plate("NE-A2"))
+        self.assertTrue(is_german_plate("NES-ZU6"))
+        self.assertFalse(is_german_plate("MÜRAB1234"))  # no hypne
+        self.assertFalse(is_german_plate("N-Ä33"))  # umlaut after hypne
+        self.assertFalse(is_german_plate("NE-A02"))  # starting 0
+        self.assertFalse(is_german_plate("NES-ZU66667"))  # too long
+
+    def test_levenshtein_distance(self):
+        self.assertEqual(levenshtein_distance("banana", "banana"), 0)
+        self.assertEqual(levenshtein_distance("banana", "ananas"), 2)
+        self.assertEqual(levenshtein_distance("banana", "bahamas"), 3)
+        self.assertEqual(levenshtein_distance("banana", "apple"), 5)
+        self.assertEqual(levenshtein_distance("", "apple"), 5)
+        self.assertEqual(levenshtein_distance("banana", ""), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 import re
 
 
-# todo: needs testing
 def is_german_plate(plate_str: str) -> bool:
+    plate_str = plate_str.upper()
     if re.match(r"^[A-ZÄÖÜ]{1,3}\-[A-Z]{1,2}[1-9]{1}[0-9]{0,3}$", plate_str):
         return True
     return False
 
 
-# todo: test
 def levenshtein_distance(s1: str, s2: str) -> int:
     if len(s2) == 0:
         return len(s1)
@@ -22,11 +21,3 @@ def levenshtein_distance(s1: str, s2: str) -> int:
             levenshtein_distance(s1, s2[1:]),
             levenshtein_distance(s1[1:], s2[1:])
         )
-
-
-"""
-print(levenshtein_distance("abc", "abc"), 0)
-print(levenshtein_distance("bca", "abc"), 2)
-print(levenshtein_distance("qwe", "abc"), 3)
-"""
-# print(levenshtein_distance("myplate34879687b876b87b6767b765", "MEB333"))
